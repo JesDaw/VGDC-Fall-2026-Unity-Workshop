ae267767cb83d4df8b86ffb9d36f3fafc1bd2ee1
Fish <thehuiq2006@gmail.com>
2026-09-17 12:35:05 -0700

    feat: installed textmeshpro packages

---
+++ Assets/Scripts/EditorTool.cs
@@ -0,0 +1,30 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+public class EditorHelper : EditorWindow
+{
+    /*
+    [MenuItem("Cool Editor/Fancy Tool")]
+    public static void ShowWindow()
+    {
+        GetWindow<EditorHelper>("State Helper");
+    }
+
+    private void OnGUI()
+    {
+        idleConstant = GUILayout.Toggle(idleConstant, "Dies");
+        if (idleConstant)
+        {
+            Debug.Log("You are dead!");
+        }
+
+        if (GUILayout.Toggle(idleConstant, "Lives"))
+        {
+            Debug.Log("You are alive!");
+        }
+    }
+    */
+}
+#endif
+
